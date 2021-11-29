119c9e85f82c99eae75dff41f204008a5096bf2b
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-11-29 10:29:19 -0300

    fix: dotenv import lint

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import dotenv from 'dotenv';
+import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const PORT = process.env.PORT || 3000;
   await app.listen(PORT);
 }

65637b539bac5938348b0dbbc8e586638c68de3d
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-11-30 10:33:38 -0300

    feat: initial user controller unit tests

--- backend/src/tests/unit/user.controller.spec.ts
+++ backend/src/tests/unit/user.controller.spec.ts
@@ -2,49 +2,121 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from '../../user/user.controller';
 import { UserService } from '../../user/user.service';
 
-describe('UserController', () => {
-  let module: TestingModule;
-  let userController: UserController;
-  let userService: UserService;
+describe('The UserController', () => {
+  let controller: UserController;
 
-  const resultAll = [
+  const users = [
     {
       id: 1,
-      nome: 'Username',
-      idade: 35,
+      nome: 'test name 1',
+      idade: 25,
+    },
+    {
+      id: 2,
+      nome: 'test name 2',
+      idade: 27,
     },
   ];
 
   const mockUserService = {
-    getAllUsers: () => resultAll,
-  };
-
-  const userServiceProvider = {
-    provide: UserService,
-    useValue: mockUserService,
+    createUser: jest.fn((createUserDTO) => {
+      return {
+        ...createUserDTO,
+      };
+    }),
+    updateUser: jest.fn((updateUserDTO) => {
+      return {
+        ...updateUserDTO,
+      };
+    }),
+    getAllUsers: jest.fn(() => users),
+    getUserById: jest.fn((id) => {
+      return users.find((user) => user.id === id);
+    }),
+    deleteUser: jest.fn((id) => {
+      return users.find((user) => user.id === id);
+    }),
   };
 
   beforeEach(async () => {
-    module = await Test.createTestingModule({
+    const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-      providers: [userServiceProvider],
-    }).compile();
+      providers: [UserService],
+    })
+      .overrideProvider(UserService)
+      .useValue(mockUserService)
+      .compile();
 
-    userService = module.get<UserService>(UserService);
-    userController = module.get<UserController>(UserController);
+    controller = module.get<UserController>(UserController);
   });
 
-  afterEach(() => {
-    jest.resetAllMocks();
+  describe('createUser function', () => {
+    it('returns an object with the created user', async () => {
+      const createUserDTO = {
+        id: 1,
+        nome: 'test name',
+        idade: 25,
+      };
+
+      const result = await controller.createUser(createUserDTO);
+
+      expect(typeof result).toBe('object');
+      expect(result).toEqual(createUserDTO);
+      expect(mockUserService.createUser).toHaveBeenCalled();
+    });
   });
 
-  describe('getAllUsers', () => {
-    it('it should return an array of users', async () => {
-      expect(userService).toBeDefined();
-      // jest
-      //   .spyOn(userService, 'getAllUsers')
-      //   .mockReturnValue(Promise.resolve(resultAll));
-      // expect(await userController.getAllUsers()).toBe(resultAll);
+  describe('updateUser function', () => {
+    it('returns an object with a message "User has been updated"', async () => {
+      const updateUserDTO = {
+        id: 1,
+        nome: 'test name',
+        idade: 25,
+      };
+
+      const result = await controller.updateUser(
+        updateUserDTO.id,
+        updateUserDTO,
+      );
+
+      expect(typeof result).toBe('object');
+      expect(result).toEqual({ message: 'User has been updated' });
+      expect(mockUserService.updateUser).toHaveBeenCalled();
+    });
+  });
+
+  describe('getAllUsers function', () => {
+    describe('when there are users', () => {
+      it('returns an array of objects containing the users', async () => {
+        const result = await controller.getAllUsers();
+
+        expect(result).toEqual(users);
+        expect(mockUserService.getAllUsers).toHaveBeenCalled();
+      });
+    });
+  });
+
+  describe('deleteUser function', () => {
+    describe('when the user exists', () => {
+      it('returns an object with a message "User has been deleted"', async () => {
+        const result = await controller.deleteUser(users[0].id);
+
+        expect(typeof result).toBe('object');
+        expect(result).toEqual({ message: 'User has been deleted' });
+        expect(mockUserService.updateUser).toHaveBeenCalled();
+      });
+    });
+  });
+
+  describe('getUserById function', () => {
+    describe('when the user exists', () => {
+      it('returns an object with the user', async () => {
+        const result = await controller.getUserById(users[0].id);
+
+        expect(typeof result).toBe('object');
+        expect(result).toEqual(users[0]);
+        expect(mockUserService.getUserById).toHaveBeenCalled();
+      });
     });
   });
 });
--- backend/src/user/schemas/user.schema.ts
+++ backend/src/user/schemas/user.schema.ts
@@ -1,7 +1,12 @@
 import * as mongoose from 'mongoose';
 
-export const UserSchema = new mongoose.Schema({
-  id: Number,
-  nome: String,
-  idade: Number,
-});
+export const UserSchema = new mongoose.Schema(
+  {
+    id: Number,
+    nome: String,
+    idade: Number,
+  },
+  {
+    versionKey: false,
+  },
+);

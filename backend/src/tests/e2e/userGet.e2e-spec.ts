090c44e796b210d6489bf601fe90ea2ca0a58e59
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-12-01 15:49:13 -0300

    feat: add /coverage-e2e to .gitnore

--- backend/src/tests/e2e/userGet.e2e-spec.ts
+++ backend/src/tests/e2e/userGet.e2e-spec.ts
@@ -1,4 +1,3 @@
-// import * as request from 'supertest';
 import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
 import { INestApplication } from '@nestjs/common';
